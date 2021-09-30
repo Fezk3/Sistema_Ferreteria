47257a27e438359360953067544832fc328a33af
Victor Segura <seguravictor57@gmail.com>
2021-09-30 14:53:16 -0600

    clase misc y cliente hereda de misc

--- cliente.py
+++ cliente.py
@@ -1,7 +1,9 @@
-class cliente:
+from misc import Misc
+
+
+class cliente(Misc):
     def __init__(self, nombre, id):
         self.nombre = nombre
         self.id = id
         self.carrito = []
 
-    
