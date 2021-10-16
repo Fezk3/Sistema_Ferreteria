765feb69081575f4aa5f3bdf13782028f12aa703
Victor Segura <seguravictor57@gmail.com>
2021-10-16 17:56:21 -0600

    merge conflict resuelto en main

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 from factura import Factura
 from producto import Producto
+from ferreteria import Ferreteria
 
 if __name__ == '__main__':
 
-<<<<<<< HEAD
-from ferreteria import Ferreteria
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-=======
     '''
     p = Producto("Leche", "12", 1000)
     fac = Factura()
     fac2 = Factura()
     p1 = Producto("tabla", "12", 1233)
->>>>>>> fact
 
     fac.recibe_item_comprado(p)
     fac.recibe_item_comprado(p1)
@@ -23,26 +16,6 @@ def print_hi(name):
     # fac.muestra_factura()
 
     '''
-<<<<<<< HEAD
-    lista = ListaDobleEnlazada()
-    lista.agregar(1)
-    lista.agregar(88)
-    lista.agregar(789)
-    print(lista.__repr__())
-    print("Eliminando")
-    lista.eliminar(0)
-    print(lista.__repr__())
-    lista.insertar(777, 1)
-    print(lista.__repr__())
-    print(f"Cantidad de nodos {lista.cantidad_nodos}")
-'''
-    '''lista = ListaDobleCircular()
-    p1 =Producto("tablas de madera",21,2000)
-    lista.agregar(p1)
-    lista.display()
-    print(lista.__repr__())'''
+    # ferre = Ferreteria()
+    # ferre.menuPrincipal()
 
-    ferre = Ferreteria()
-    ferre.menuPrincipal()
-=======
->>>>>>> fact
