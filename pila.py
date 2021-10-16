3057ae87870cd6c4e27a88af11337a6fa9352f4a
Victor Segura <seguravictor57@gmail.com>
2021-10-15 18:44:07 -0600

    adicion de clase pila

---
+++ pila.py
@@ -0,0 +1,16 @@
+class Pila:
+
+    def init(self):
+        self.items = []
+
+    def esta_vacia(self):
+        return self.items == []
+
+    def agreagar(self, item):
+        self.items.insert(0, item)
+
+    def sacar(self):
+        return self.items.pop()
+
+    def tamnaio(self):
+        return len(self.items)
