82d7245a2a6fa1dcf75cb6d49fe778b5273aa5a4
Victor Segura <seguravictor57@gmail.com>
2021-10-16 15:48:32 -0600

    metodo para calcular total de los procutos del carrito

--- cliente.py
+++ cliente.py
@@ -10,4 +10,11 @@ class Cliente:
     def agregar_producto_carrito(self, prod):
         self.carrito.agreagar(prod)
         print(f"Elemento agregado al carrito: {prod}")
-    
+
+    def total_carrito(self):
+        total = 0
+        for n in self.carrito.items:
+            total += n.precio
+
+        return total
+
