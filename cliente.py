3ab83e7a40d72efd07d54e5d3712e7ac024576d0
Victor Segura <seguravictor57@gmail.com>
2021-10-16 15:43:26 -0600

    cambio de nombre a funcion para agregar productos al carrito

--- cliente.py
+++ cliente.py
@@ -1,7 +1,13 @@
+from pila import Pila
+
 class Cliente:
 
     def __init__(self, nombre, id):
         self.nombre = nombre
         self.id = id
-        self.carrito = []
+        self.carrito = Pila()  # se aniadiran los productos de las pilas que los contienen en la ferreteria
 
+    def agregar_producto_carrito(self, prod):
+        self.carrito.agreagar(prod)
+        print(f"Elemento agregado al carrito: {prod}")
+    
