5a18061cb8664f64f8a8152118fc4e33015edc4a
emanuel soto <manusl2908@gmail.com>
2021-10-16 22:53:26 -0600

    cambios

--- sucursal.py
+++ sucursal.py
@@ -1,6 +1,7 @@
 from lista_doble_C import ListaDobleCircular
 from seccion import Seccion
 
+
 class Sucursal:
     def __init__(self, ubicacion, numero):
         self.id = numero
@@ -8,30 +9,19 @@ class Sucursal:
         self.listaSeccion = ListaDobleCircular()
 
     def __repr__(self):
-        salida=''
+        salida = ''
         salida += f'Identificacion {self.id}\nUbicacion: {self.ubicacion}\nSecciones de la sucursal: {self.listaSeccion.__repr__()}\n'
-        #salida +=
+        # salida +=
         return salida
 
-    def agregaSeccion(self,seccion):
+    def agregaSeccion(self, seccion):
         self.listaSeccion.agregar(seccion)
 
-
     def agregaSeccionEspecifica(self, indice, seccion):
-        numero = self.listaSeccion.num(indice)
-        if numero != None:
-            self.listaSeccion.insertar(seccion,numero)
-        return numero
+        self.listaSeccion.insertar(seccion, indice)
 
     def eliminarSeccion(self, indice):
-        numero = self.listaSeccion.num(indice)
-        if numero != None:
-            self.listaSeccion.eliminar(numero)
-        return numero
+        self.listaSeccion.eliminar(indice)
 
     def retornarSeccion(self, indice):
-        numero = self.listaSeccion.num(indice)
-        if numero != None:
-            return self.listaSeccion.get(numero)
-        return numero
-
+        return self.listaSeccion.get(indice)
