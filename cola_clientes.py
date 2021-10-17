bbad7045b491b5b0adcd3a1bb9d9b7e9c9287927
Victor Segura <seguravictor57@gmail.com>
2021-10-17 10:58:57 -0600

    nueva clase ColaClientes

---
+++ cola_clientes.py
@@ -0,0 +1,24 @@
+from cliente import Cliente
+from producto import Producto
+
+class ColaClientes:
+
+    def __init__(self):
+        self.cola_clientes = []
+        # esto para que cada cola de clientes tenga uno frente al user 
+        c = Cliente("Carlos", "123")
+        c.carrito.agreagar(Producto("Puerta Pino", "6", 90000))
+        self.agregar_cliente(c)
+
+    def agregar_cliente(self, clien):
+        self.cola_clientes.append(clien)
+
+    def sacar_cliente(self):
+        self.cola_clientes.pop(0)
+
+    def esta_vacia(self):
+        return self.cola_clientes == []
+
+    def get_tamanio(self):
+        return len(self.cola_clientes)
+
